96ebda989c2388e48f2264ea8f3e04fd44a84ee1
Mohammed Khattabi <m.s.khattabi1@gmail.com>
2020-03-24 21:52:31 +0300

    MEE

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace FirstCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-                        Console.WriteLine("Hello");
+                        Console.WriteLine("Welcome");
         }
     }
 }
