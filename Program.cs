c7a4fcb2d9707f05c9308defa87d8c552fa8c59f
Mohammed Khattabi <m.s.khattabi1@gmail.com>
2020-03-26 05:04:58 +0300

    If else

--- Program.cs
+++ Program.cs
@@ -6,8 +6,26 @@ namespace FirstCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-                        Console.WriteLine("Welcome Mother");
+            string place = "Coding Dojo";
+            string message = $"Hello {place}";
+        //    Console.WriteLine("Using Console.WriteLine, you can output any string to the terminal/console");
+            Console.WriteLine(message);
+        //    Console.WriteLine($"My name is {0}, I am " + 28 + " years old", "Tim");
+
+            int numRings =3;
+if (numRings >= 5)
+{
+    Console.WriteLine("You are welcome to join the party");
+}
+else if (numRings > 2)
+{
+    Console.WriteLine($"Decent...but {numRings} rings aren't enough");
+}
+else
+{
+    Console.WriteLine("Go win some more rings");
+}
+
         }
     }
-}
+}  
